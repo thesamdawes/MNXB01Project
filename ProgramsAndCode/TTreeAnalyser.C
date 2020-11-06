33bae0c3dae63dbcf700bf575db70270ff67369d
Tom Causer <TomMCauser@gmail.com>
2020-11-06 09:10:21 +0100

    Added analysis tool that uses TTree to analyse.

---
+++ ProgramsAndCode/TTreeAnalyser.C
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <TF1.h> // 1d function class
+#include <TH1.h> // 1d histogram classes
+#include <TStyle.h>  // style object
+#include <TMath.h>   // math functions
+#include <TCanvas.h> // canvas object
+#include <vector>
+#include <TGraphPainter.h>
+#include <TGraph.h>
+#include <TFile.h>
+#include <TTree.h>
+
+using namespace std;
+
+void DataFile()
+	{
+		fstream file;
+		file.open("CleansedLundData.csv", ios::in);
+		int YYYY, MM, DD, hh, mm, ss;
+		double Temp;
+		char t, f;
+		
+		TFile *output = new TFile("Test.root", "RECREATE");
+		
+		TTree *tree = new TTree("tree", "tree");
+		
+		tree->Branch("YYYY", &YYYY, "YYYY/I");
+		tree->Branch("MM", &MM, "MM/I");
+		tree->Branch("DD", &DD, "DD/I");
+		tree->Branch("mm", &mm, "mm/I");
+		tree->Branch("hh", &hh, "hh/I");
+		tree->Branch("ss", &ss, "ss/I");
+		tree->Branch("Temp", &Temp, "Temp/D");
+				
+		while (1)
+			{
+				file >> YYYY >> t >> MM >> t >> DD >> hh >> f >> mm >> f >> ss >> Temp;
+				if(file.eof()) break;
+				//cout << YYYY << t << MM << t << DD << " " << hh << ":" << mm << ":" << ss << " " << Temp << endl;
+				tree->Fill();		
+			}
+			
+		//tree->Draw("Temp"); do this for each location and do analysis
+		
+		tree->Draw("Temp:YYYY", "MM == 7");
+			
+		output->Write();
+		output->Close();		
+		file.close();		
+	}
